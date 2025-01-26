f0fb03079c4390cce99902f011cfb7baf0f082c4
Daniel Nascimento <danielnserpa@live.com>
2025-01-26 15:10:34 +0000

    "Variables-and-Constants"

---
+++ src/main/java/fundamentals/AreaCircunferencia.java
@@ -0,0 +1,13 @@
+package fundamentals;
+
+public class AreaCircunferencia {
+
+    public static void main(String[] args) {
+        double raio = 3.56;
+        final double PI = 3.141594; // constants use CAPITAL letters
+        double area = raio * raio * PI;
+
+        System.out.println(area);
+
+    }
+}
--- src/main/java/fundamentals/FirstProgram.java
+++ src/main/java/fundamentals/FirstProgram.java
@@ -1,4 +1,4 @@
-package exercises;
+package fundamentals;
 
 public class FirstProgram {
     public static void main(String[] args) {
