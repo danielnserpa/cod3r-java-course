1b5a2dd9f9133a9cc5722b32ce42d7810007d1cb
Daniel Nascimento <danielnserpa@live.com>
2025-01-28 17:51:07 +0000

    "Primitive-Types"

---
+++ src/main/java/fundamentals/PrimitiveTypes.java
@@ -0,0 +1,23 @@
+package fundamentals;
+
+public class PrimitiveTypes {
+
+    public static void main(String[] args) {
+
+        // Whole Numerals
+        byte years = 23;
+        short flightNumbers = 523;
+        int id = 454545;
+        long number = 923723723273L;
+
+        // Real Numerals
+        float breaks = 82373.4F;
+        double huge = 83748374834.9349834;
+
+        // Boolean
+        boolean yes = true;
+
+        //Character
+        char status = 'A';
+    }
+}
