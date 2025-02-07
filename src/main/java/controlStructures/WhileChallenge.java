0be1d850fae32db5221b2fdc06990c99d23f67b8
Daniel Nascimento <danielnserpa@live.com>
2025-02-07 10:28:44 +0000

    "While-Challenge"

---
+++ src/main/java/controlStructures/WhileChallenge.java
@@ -0,0 +1,25 @@
+package controlStructures;
+
+import java.util.Scanner;
+
+public class WhileChallenge {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Tell me something: ");
+        String answer = scanner.nextLine();
+        System.out.println(answer);
+
+        while (!answer.equalsIgnoreCase("exit")) {
+            System.out.println("\n" + "What else do you want to say? ");
+            answer = scanner.nextLine();
+
+            if (answer.equalsIgnoreCase("exit")) {
+                System.out.println("Bye :)");
+            } else {
+                System.out.println(answer);
+            }
+        }
+        scanner.close();
+    }
+}
