71539fc831088e3dba3c0b37dcfeaf48d45868c4
Daniel Nascimento <danielnserpa@live.com>
2025-02-11 13:04:11 +0000

    "Date-String-format-return-Method"

--- src/main/java/classesAndMethods/Date.java
+++ src/main/java/classesAndMethods/Date.java
@@ -3,15 +3,26 @@ package classesAndMethods;
 public class Date {
 
     int day;
-    String month;
+    int month;
     int year;
 
     public Date() {
 
     }
-    public Date (int day, String month, int year) {
+    public Date (int day, int month, int year) {
         this.day = day;
         this.month = month;
         this.year = year;
     }
+
+    String formattedDate(int day, int month, int year) {
+
+            return String.format("\n%d, %d, %d",day, month, year);
+    }
+
+    String formattedDate(){
+        return String.format("\n%d, %d, %d",day, month, year);
+    }
+
+
 }
--- src/main/java/classesAndMethods/DateTest.java
+++ src/main/java/classesAndMethods/DateTest.java
@@ -6,25 +6,27 @@ public class DateTest {
 
         Date d1 = new Date();
         Date d2 = new Date();
-        Date d3 = new Date(23, "april", 1984);
+        Date d3 = new Date(23, 4, 1984);
 
         d1.day = 22;
-        d1.month = "feb";
+        d1.month = 2;
         d1.year = 1995;
 
         d2.day = 11;
-        d2.month = "march";
+        d2.month = 3;
         d2.year = 1997;
 
         System.out.println(d1.day + " " +  d1.month + " " + d1.year);
-        System.out.printf("%d %s %d", d1.day, d1.month, d1.year);
+        System.out.printf("%d %d %d", d1.day, d1.month, d1.year);
 
         System.out.println("\n" + d2.day + " " + d2.month + " " + d2.year);
-        System.out.printf("%d %s %d", d2.day, d2.month, d2.year);
-
+        System.out.printf("%d %d %d", d2.day, d2.month, d2.year);
 
         System.out.println("\n" + d3.day + " " + d3.month + " " + d3.year);
-        System.out.printf("%d %s %d", d3.day, d3.month, d3.year);
+        System.out.printf("%d %d %d", d3.day, d3.month, d3.year);
+
+        System.out.println(d1.formattedDate(d1.day, d1.month, d1.year));
+        System.out.println(d3.formattedDate());
 
 
     }
