df11f09fb54b33a0b2cdd95d7093689bf03cda5a
Daniel Nascimento <danielnserpa@live.com>
2025-02-12 11:01:47 +0000

    "ValueVsReference"

---
+++ src/main/java/classesAndMethods/FirstTrauma.java
@@ -0,0 +1,16 @@
+package classesAndMethods;
+
+public class FirstTrauma {
+
+    int a = 3;
+    static int b = 4;
+    public static void main(String[] args) {
+
+        FirstTrauma fT = new FirstTrauma();
+
+        System.out.println(fT.a);
+
+        System.out.println(b);
+
+    }
+}
---
+++ src/main/java/classesAndMethods/ValueVsReference.java
@@ -0,0 +1,36 @@
+package classesAndMethods;
+
+public class ValueVsReference {
+    public static void main(String[] args) {
+
+        double a = 2;
+        double b = a; // atribuicao por valor pois sao tipos primitivos
+
+        a++;
+        b--;
+
+        System.out.println(a + " " + b );
+
+        Date d1 = new Date(1, 6, 2022);
+        Date d2 = d1; // atribuicao por referencia pois é objeto
+
+        d1.day = 31;
+        d2.month = 12;
+
+        System.out.println(d1.formattedDate());
+        System.out.println(d2.formattedDate());
+
+        backToInitialDate(d1);
+
+        System.out.println(d1.formattedDate());
+        System.out.println(d2.formattedDate());
+
+        // Por conta de ter atribuido d2 = d1 no inicio, eles apontam pro mesmo local de memoria. Modificando um, o outro é modificado automaticamente.
+    }
+
+    static void backToInitialDate(Date d) {
+        d.day = 1;
+        d.month = 1;
+        d.year = 1970;
+    }
+}
