103277d27e67f90f60d7642850d1f1914da7f138
Daniel Nascimento <danielnserpa@live.com>
2025-03-07 10:49:09 +0000

    "Lambdas-Foreach"

--- src/main/java/lambdas/Forach.java
+++ src/main/java/lambdas/Forach.java
@@ -1,12 +1,19 @@
 package lambdas;
 
 
+import arraysandcollections.arrays.Foreach;
+
 import java.util.Arrays;
 import java.util.List;
 
 
 
 public class Forach {
+
+    static void print(String name) {
+        System.out.println("My name is " + name);
+    }
+
     public static void main(String[] args) {
         List<String> approved = Arrays.asList("Ana", "Bia", "Carlos", "Gui");
 
@@ -15,10 +22,16 @@ public class Forach {
         }
 
         System.out.println("\nWith Lamba #01: ");
-        approved.forEach(nome -> System.out.println(nome));
+        approved.forEach(aprovados -> System.out.println(aprovados));
+
+        System.out.println("\nWith Lamba #02: ");
+        approved.forEach(aprovados -> print(aprovados));
 
         System.out.println("\nWith Method reference #01: ");
         approved.forEach(System.out::println);
+
+        System.out.println("\nWith Method reference #02: ");
+        approved.forEach(Forach::print);
 
     }
 }
