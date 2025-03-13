c5d9bf713012b4436988c1351b3d4162b20527fe
DanielnSerpa <danielnserpa@live.com>
2025-03-13 11:29:09 +0000

    "List-Iterator-Stream"

--- src/main/java/streams/PrintingObjects.java
+++ src/main/java/streams/PrintingObjects.java
@@ -1,8 +1,30 @@
 package streams;
 
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class PrintingObjects {
 
     public static void main(String[] args) {
+
+        List<String> approved = Arrays.asList("Ana", "Pedro", "Lucas", "Gui");
+
+        for (int i = 0; i < approved.size(); i++) {
+            System.out.println(approved.get(i));
+        }
+
+        System.out.println(" ");
+
+        Iterator<String> iterator = approved.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+
+        System.out.println(" ");
+
+        Stream<String>
 
 
 
