18e859a2639b8ca34c07e12e06acc41ea3009c97
DanielnSerpa <danielnserpa@live.com>
2025-03-15 09:53:39 +0000

    "Streams-Map-Challenge"

---
+++ src/main/java/streams/MapChallenge.java
@@ -0,0 +1,27 @@
+package streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class MapChallenge {
+    public static void main(String[] args) {
+
+        /*
+
+         */
+
+        Consumer<Integer> print = System.out::println;
+
+        List<Integer> nums = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+        Function<Integer, String> convertToBinary = n -> Integer.toBinaryString(n);
+        Function<String, String> reverse = n -> new StringBuilder(n).reverse().toString();
+        Function<String, Integer> convertToInteger = n -> Integer.parseInt(n, 2);
+
+        nums.stream().map(convertToBinary).map(reverse).map(convertToInteger).forEach(print);
+
+//        UnaryOperator<Integer> convertToInteger = n ->
+    }
+}
