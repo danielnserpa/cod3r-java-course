d1936180e3e4e39166460b517d704ae368d5883a
DanielnSerpa <danielnserpa@live.com>
2025-03-22 09:01:19 +0000

    "Null-Pointer-Exception"

--- src/main/java/errorhandling/Basic.java
+++ src/main/java/errorhandling/Basic.java
@@ -1,9 +1,18 @@
 package errorhandling;
 
 public class Basic {
+
+    public static void printStudentsName(Student student){
+        System.out.println(student.name);
+    }
+
     public static void main(String[] args) {
 
+        Student s1 = null;
+        printStudentsName(s1);
 
-        System.out.println(7/0);
+
+//        System.out.println(7/0);
     }
+
 }
---
+++ src/main/java/errorhandling/Student.java
@@ -0,0 +1,20 @@
+package errorhandling;
+
+public class Student {
+
+    public final String name;
+    public final double grade;
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", grade=" + grade +
+                '}';
+    }
+
+    public Student(String name, double grade) {
+        this.name = name;
+        this.grade = grade;
+    }
+}
