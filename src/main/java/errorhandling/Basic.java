986256db1d0a657a0c8db85c2ed8bd7698aceeb6
DanielnSerpa <danielnserpa@live.com>
2025-03-22 08:59:36 +0000

    "Arithmetic-Exception"

---
+++ src/main/java/errorhandling/Basic.java
@@ -0,0 +1,9 @@
+package errorhandling;
+
+public class Basic {
+    public static void main(String[] args) {
+
+
+        System.out.println(7/0);
+    }
+}
--- src/main/java/errorhandling/Error.java
+++
@@ -1,4 +0,0 @@
-package errorhandling;
-
-public class Error {
-}
