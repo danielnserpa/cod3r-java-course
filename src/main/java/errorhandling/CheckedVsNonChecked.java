be9d1da5456a7793bc061f328f4a8ebb11446ed8
DanielnSerpa <danielnserpa@live.com>
2025-03-24 10:16:35 +0000

    "Checked-and-Non-Checked-Error-Handling"

--- src/main/java/errorhandling/CheckedVsNonChecked.java
+++ src/main/java/errorhandling/CheckedVsNonChecked.java
@@ -22,11 +22,13 @@ public class CheckedVsNonChecked {
 
     }
 
+    // Non checked doesn't need to be treated necessarily
     static void generateError1() {
         throw new RuntimeException("There's an error #01");
 
     }
 
+    // Checked has to be treated and used throws
     static void generateError2() throws Exception {
         throw new Exception("There's another error #2");
     }
---
+++ src/main/java/errorhandling/PersonalizedExceptionA/NegativeNumberException.java
@@ -0,0 +1,16 @@
+package errorhandling.PersonalizedExceptionA;
+
+public class NegativeNumberException extends RuntimeException {
+
+    private String name;
+
+    public NegativeNumberException(String name){
+        this.name = name;
+    }
+
+    @Override
+    public String getMessage() {
+        return String.format("The atribute %s is negative", name);
+
+    }
+}
