ac699d522e34314a9a5acda2c77a88b5d8555412
DanielnSerpa <danielnserpa@live.com>
2025-04-29 19:07:15 +0100

    "Checked and Non Checked Errors"

--- src/main/java/errorhandling/CheckedVsNonChecked.java
+++ src/main/java/errorhandling/CheckedVsNonChecked.java
@@ -8,7 +8,6 @@ public class CheckedVsNonChecked {
             generateError1();
         } catch (Exception e) {
             System.out.println(e.getMessage());
-
         }
 
         try {
@@ -18,11 +17,10 @@ public class CheckedVsNonChecked {
         }
 
         System.out.println("The end");
-
-
     }
 
     // Non checked doesn't need to be treated necessarily
+    // Tem a liberdade de "voce quer ou nao" tratar o erro
     static void generateError1() {
         throw new RuntimeException("There's an error #01");
 
