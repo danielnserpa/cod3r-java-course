e126b1dbd0b6d73efa9c0f9ba7463f51fdc827d1
DanielnSerpa <danielnserpa@live.com>
2025-05-25 10:05:18 +0200

    "Game"

--- src/main/java/generics/Gui.java
+++ src/main/java/generics/Gui.java
@@ -6,24 +6,17 @@ public class Gui {
 
     public static void main(String[] args) {
 
-
-
         int randomNumber = (int)(Math.random() * 5) + 1;
         int guess = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter a number from 1 to 5"));
 
-        while (randomNumber != guess) {
-
-            if (randomNumber == guess) {
-            JOptionPane.showMessageDialog(null, "Congrats, you guessed the right number");
-        } else {
-            JOptionPane.showMessageDialog(null, "That's not the right number. Try again");
-        }
+        while (guess != randomNumber) {
+            JOptionPane.showMessageDialog(null, "Wrong guess. Try again!");
+            guess = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter a number from 1 to 5"));
         }
 
-
-
-
+        JOptionPane.showMessageDialog(null, "Congrats, you guessed the right number: " + randomNumber);
     }
-
 }
+
+
 
